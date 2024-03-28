aee71b483dc474b9845424251e084dca9250f594
Ulvi  :() <112339223+UlviPoladov@users.noreply.github.com>
2024-03-28 06:19:32 +0400

    Add project files.

---
+++ ClientSide/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace ClientSide
+{
+    public partial class MainWindow : Window
+    {
+        private Socket clientSocket;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string clientName = clientNameTextBox.Text;
+                byte[] data = Encoding.ASCII.GetBytes("[NAME]" + clientName);
+                IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("192.168.0.105"), 5357);
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                clientSocket.SendTo(data, serverEP);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void Button_Send_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string message = clientMessageTextBox.Text;
+                byte[] data = Encoding.ASCII.GetBytes("[MESSAGE]" + message);
+                IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("192.168.0.105"), 5357);
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                clientSocket.SendTo(data, serverEP);
+                clientMessageTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ServerSide/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace ServerSide
+{
+    public partial class MainWindow : Window
+    {
+        private Socket serverSocket;
+        private Thread receiveThread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupServer();
+        }
+
+        private void SetupServer()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 5357);
+            serverSocket.Bind(endPoint);
+
+            receiveThread = new Thread(new ThreadStart(ReceiveData));
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+        }
+
+        private void ReceiveData()
+        {
+            byte[] buffer;
+            IPEndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint remoteEndPoint = (EndPoint)senderEndPoint;
+            while (true)
+            {
+                buffer = new byte[1024];
+                try
+                {
+                    int receivedLength = serverSocket.ReceiveFrom(buffer, ref remoteEndPoint);
+                    string receivedMessage = Encoding.ASCII.GetString(buffer, 0, receivedLength);
+                    if (receivedMessage.StartsWith("[NAME]"))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            connectedname.Content = receivedMessage.Substring(6);
+                        });
+                    }
+                    else if (receivedMessage.StartsWith("[MESSAGE]"))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            serverListBox.Items.Add("Username: " + connectedname.Content + " Message: " + receivedMessage.Substring(9));
+                        });
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show("Socket error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
